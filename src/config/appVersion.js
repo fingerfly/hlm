9504f1395a5d40a6c934f6269f0ea20a3151884b
hlm-release <hlm-release@local.invalid>
2026-03-19 22:09:54 +0800

    Release v4.4.1 (8)

--- src/config/appVersion.js
+++ src/config/appVersion.js
@@ -5,7 +5,7 @@
  * - Exposes formatted display string for version badge.
  */
 export const APP_VERSION = "4.4.1";
-export const APP_BUILD = 7;
+export const APP_BUILD = 8;
 
 /**
  * Build display label used in UI version badge.
